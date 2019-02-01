67e1579db870e155a3cc755de9da00e30287eb48
Jonathan Gin <jonathan.gin@shopify.com>
2019-02-01 16:05:20 -0500

    Sample random elements from array to create deck

--- commands/generate.js
+++ commands/generate.js
@@ -1,4 +1,29 @@
+const { red, yellow, magenta, cyan, white } = require('cli-color');
+const cards = require('../data/cards.json');
+const { communications, force, finance, specialInterest } = cards;
+const log = console.log;
+
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+Array.prototype.sample = function(count = 1) {
+  return shuffle(this).slice(0, count);
+}
+
 module.exports = (args) => {
-  console.log('generating game');
+  log('generating game');
+  const deck = [];
+  //log(cards);
+  deck.push(communications.sample());
+  deck.push(force.sample());
+  deck.push(finance.sample());
+  deck.push(specialInterest.sample(2));
+
+  log(deck);
 };
 
