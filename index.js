c74538f3f4ae785123be0ab49beb89bc514f56d5
Jonathan Gin <jonathan.gin@shopify.com>
2019-02-01 01:29:29 -0500

    Parse basic commands, default to generate when no arguments given

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const minimist = require('minimist');
+const generate = require('./commands/generate');
+const load = require('./commands/load');
+
 module.exports = () => {
-  console.log('Generating your coup game')
-}
+  const args = minimist(process.argv.slice(2));
+  let cmd = args._[0] || 'generate';
+
+  switch (cmd) {
+    case 'generate':
+      generate();
+      break;
+    case 'load':
+      load(args);
+      break;
+    default:
+      console.error(`"${cmd}" is not a valid command!`);
+      break;
+  }
+};
