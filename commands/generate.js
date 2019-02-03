ff13e51d0960d5e5739e3ee31a60d52f62306101
Maas Lalani <maaslalani1@gmail.com>
2019-02-03 12:11:22 -0500

    Add log as a function from console

--- commands/generate.js
+++ commands/generate.js
@@ -5,6 +5,9 @@ const { red, yellow, magenta, cyan, white } = require('cli-color');
 const cards = require('../data/cards.json');
 const { communications, force, finance, specialInterest } = cards;
 
+// Functions
+const { log } = console;
+
 const shuffle = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -15,11 +18,10 @@ const shuffle = (array) => {
 
 Array.prototype.sample = function(count = 1) {
   return shuffle(this).slice(0, count);
-};
+}
 
 module.exports = (args) => {
   let deck = [];
-  let seed = "";
 
   deck.push(communications.sample());
   deck.push(force.sample());
@@ -37,10 +39,5 @@ module.exports = (args) => {
     console.log("Action: " + deck[i].action);
     deck[i].counteraction && console.log("Counteraction: " + deck[i].counteraction);
   }
-
-  // Generate game seed
-  deck.forEach((card) => seed += card.id);
-  log(`Your game seed is '${seed}.'`);
-
 };
 
