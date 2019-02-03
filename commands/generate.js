7f8bc0ff00827f2ce88e51a866705363acf408d7
Paarth Madan <paarth.madan@shopify.com>
2019-02-03 12:59:56 -0500

    Added game object, refactored seed by removing symbols

--- commands/generate.js
+++ commands/generate.js
@@ -8,22 +8,37 @@ const { communications, force, finance, specialInterest } = cards;
 // Functions
 const { log } = console;
 
-const game = [
-  {"c": communications},
-  {"f": force},
-  {"$": finance},
-  {"s": specialInterest},
-  {"s": specialInterest},
-];
+const game = {
+  "Communications": {
+    "count": 1,
+    "colour": cyan,
+    "cards": communications,
+  },
+  "Force": {
+    "count": 1,
+    "colour": yellow,
+    "cards": force,
+  },
+  "Finance": {
+    "count": 1,
+    "colour": red,
+    "cards": finance,
+  },
+  "Special Interest": {
+    "count": 2,
+    "colour": magenta,
+    "cards": specialInterest,
+  },
+};
   
 
 const random = (n) => Math.floor(Math.random() * n); 
 
 const generateGameSeed = () => {
   var s = "";
-  for (const character of game) {
-    for (const symbol in character) {
-      s += `${symbol}${random(character[symbol].length)}`
+  for (const character in game) {
+    for (let i = 0; i < game[character]["count"]; i++) {
+      s += random(game[character]["cards"].length);
     }
   }
   return s;
