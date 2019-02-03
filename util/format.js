cba6a58a125815b1ce3256fbb5683c4d4b53e850
Maas Lalani <maaslalani1@gmail.com>
2019-02-03 15:03:41 -0500

    Bold actions and counteractions

--- util/format.js
+++ util/format.js
@@ -1,5 +1,5 @@
 // NPM modules
-const { red, yellow, magenta, cyan, white } = require('cli-color');
+const { bold } = require('cli-color');
 const game = require('./game');
 
 const cards = ['Communications', 'Force', 'Finance', 'Special Interest', 'Special Interest'];
@@ -7,8 +7,8 @@ const cards = ['Communications', 'Force', 'Finance', 'Special Interest', 'Specia
 module.exports = (deck) => {
   for (let i = 0; i < cards.length; i++) {
     console.log(game[cards[i]].colour(cards[i]));
-    console.log('Action: ' + deck[i].action);
-    deck[i].counteraction && console.log('Counteraction: ' + deck[i].counteraction);
+    console.log(bold('Action: ') + deck[i].action);
+    deck[i].counteraction && console.log(bold('Counteraction: ') + deck[i].counteraction);
   }
 };
 
