2b77e44199a373ed926d4e811c8b9a454864c18e
Jonathan Gin <jonathan.gin@shopify.com>
2019-02-03 12:50:24 -0500

    Add load functionality

--- commands/load.js
+++ commands/load.js
@@ -1,4 +1,34 @@
-module.exports = (args) => {
-  console.log('loading game');
+const { communications, force, finance, specialInterest } = require('../data/cards.json');
+const log = console.log;
+
+module.exports = (seed) => {
+  console.log('Loading game');
+  log(seed);
+  const deck = [];
+
+  const ids = seed.split('');
+  ids.forEach((id, index) => {
+    switch (index) {
+      case 0:
+        deck.push(communications[id]);
+        break;
+      case 1:
+        deck.push(force[id]);
+        break;
+      case 2:
+        deck.push(finance[id]);
+        break;
+      case 3:
+        deck.push(specialInterest[id]);
+        break;
+      case 4:
+        deck.push(specialInterest[id]);
+        break;
+      default:
+        log('Your seed is too long');
+        break;
+    }
+  });
+  log(deck);
 };
 
