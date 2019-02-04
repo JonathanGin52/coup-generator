fb70357b85e4236ec7749c0510fd693212f7bd02
Paarth Madan <paarth.madan@shopify.com>
2019-02-04 13:00:42 -0500

    Change real to alias in format.js

--- util/format.js
+++ util/format.js
@@ -9,7 +9,7 @@ module.exports = (deck) => {
     for (let repetitions = 0; repetitions < game[card].count; repetitions++) {
       let i = cardIndex + repetitions;
       console.log(horizontalLine());
-      console.log(game[card].colour(`${card}: ${deck[i].name} (${game[card].real[repetitions]})`));
+      console.log(game[card].colour(`${card}: ${deck[i].name} (${game[card].alias[repetitions]})`));
       console.log(bold('Action: ') + deck[i].action);
       deck[i].counteraction && console.log(bold('Counteraction: ') + deck[i].counteraction);
     }
