88f4268c736c13e4a0dc7c842a10b9615c8c4ac8
Paarth Madan <paarth.madan@shopify.com>
2019-02-04 12:53:32 -0500

    Added real characters to JSON

--- util/game.js
+++ util/game.js
@@ -4,21 +4,25 @@ const { red, yellow, magenta, cyan, white } = require('cli-color');
 const game = {
   'Communications': {
     count: 1,
+    real: ['Captain'],
     colour: cyan,
     cards: communications,
   },
   'Force': {
     count: 1,
+    real: ['Assassin'],
     colour: yellow,
     cards: force,
   },
   'Finance': {
     count: 1,
+    real: ['Duke'],
     colour: red,
     cards: finance,
   },
   'Special Interest': {
     count: 2,
+    real: ['Contessa', 'Ambassador'],
     colour: magenta,
     cards: specialInterest,
   },
