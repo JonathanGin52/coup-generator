ca9e133735ad062689e9bf90a544491975b497e6
Paarth Madan <paarth.madan@shopify.com>
2019-02-04 08:11:39 -0500

    Fixed SED issue, replaced count -> repetitions by mistake

--- util/format.js
+++ util/format.js
@@ -5,7 +5,7 @@ const game = require('./game');
 module.exports = (deck) => {
   let cardIndex = 0;
   for (let card in game) {
-    for (let repetitions = 0; repetitions< game[card].repetitions; repetitions++) {
+    for (let repetitions = 0; repetitions < game[card].count; repetitions++) {
       let i = cardIndex + repetitions;
       console.log(game[card].colour(`${card}: ${deck[i].name}`));
       console.log(bold('Action: ') + deck[i].action);
