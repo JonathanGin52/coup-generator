4653007bc8beff7b6fd214f5eb7a97d0bae65e02
Paarth Madan <paarth.madan@shopify.com>
2019-02-04 07:43:34 -0500

    Added horizontal seperator between cards, for easy distinction

--- util/format.js
+++ util/format.js
@@ -3,9 +3,11 @@ const { bold } = require('cli-color');
 const game = require('./game');
 
 const cards = ['Communications', 'Force', 'Finance', 'Special Interest', 'Special Interest'];
+const horizontalLine = () => '—'.repeat(process.stdout.columns);
 
 module.exports = (deck) => {
   for (let i = 0; i < cards.length; i++) {
+    console.log(horizontalLine());  
     console.log(game[cards[i]].colour(`${cards[i]}: ${deck[i].name}`));
     console.log(bold('Action: ') + deck[i].action);
     deck[i].counteraction && console.log(bold('Counteraction: ') + deck[i].counteraction);
