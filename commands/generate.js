1a245be002e3320a86c940576f0477d5f6a7205f
Jocelyn Lim <jocelynlim@bell.net>
2019-02-04 22:03:00 -0500

    (maybe) fixed bug where special interest cards repeat

--- commands/generate.js
+++ commands/generate.js
@@ -7,12 +7,22 @@ const random = (n) => Math.floor(Math.random() * n);
 const generateGameSeed = () => {
   let seed = '';
   for (const character in game) {
+
+    let last = '';
+
     for (let i = 0; i < game[character]['count']; i++) {
-      seed += random(game[character]['cards'].length);
+
+      while (true){
+        seed += random(game[character]['cards'].length);
+        if (seed != last){
+          break;
+        seed = last;
+
+      }
     }
+  }
   }
   return seed;
 };
-
 module.exports = (args) => load(generateGameSeed());
 
