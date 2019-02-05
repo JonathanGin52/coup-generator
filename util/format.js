2cd7a3934d357a73f18017aee987a0960848b9cd
Maas Lalani <maaslalani1@gmail.com>
2019-02-05 10:53:19 -0500

    Destructure log from console

--- util/format.js
+++ util/format.js
@@ -1,6 +1,7 @@
 // NPM modules
-const { bold } = require('cli-color');
 const game = require('./game');
+const { bold } = require('cli-color');
+const { log } = console;
 const horizontalLine = () => '—'.repeat(process.stdout.columns);
 
 module.exports = (deck) => {
@@ -8,10 +9,10 @@ module.exports = (deck) => {
   for (let card in game) {
     for (let repetitions = 0; repetitions < game[card].count; repetitions++) {
       let i = cardIndex + repetitions;
-      console.log(horizontalLine());
-      console.log(game[card].colour(`${card}: ${deck[i].name} (${game[card].alias[repetitions]})`));
-      console.log(bold('Action: ') + deck[i].action);
-      deck[i].counteraction && console.log(bold('Counteraction: ') + deck[i].counteraction);
+      log(horizontalLine());
+      log(game[card].colour(`${card}: ${deck[i].name} (${game[card].alias[repetitions]})`));
+      log(bold('Action: ') + deck[i].action);
+      deck[i].counteraction && log(bold('Counteraction: ') + deck[i].counteraction);
     }
     cardIndex++;
   }
