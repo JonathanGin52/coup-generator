362703535dba6aa49cb072bdd12bdd25d5bee95d
Maas Lalani <maaslalani1@gmail.com>
2019-02-05 11:41:28 -0500

    Handle errors and error messages

--- commands/load.js
+++ commands/load.js
@@ -2,6 +2,7 @@ const { communications, force, finance, specialInterest } = require('../data/car
 const { underline } = require('cli-color');
 const formatOutput = require('../util/format');
 const error = require('../util/error');
+const { SEED_LENGTH, RANGE_MINIMUM, RANGE_MAXIMUM, RANGE_ERROR, PARSE_ERROR, LENGTH_ERROR } = require('../util/validation');
 
 module.exports = (seed) => {
   console.log(`Your game seed is ${underline(seed)}`);
@@ -9,19 +10,21 @@ module.exports = (seed) => {
   const seedArray = seed.split('');
 
   // Validate game seed
-  if (seed.length !== 5) {
-    error(`Seed length is too ${seed.length > 5 ? 'long' : 'short'}, seed length must be 5.`, true);
-  }
+  seed.length !== SEED_LENGTH && error(LENGTH_ERROR, true);
+  !/^\d+$/.test(seed) && error(PARSE_ERROR, true);
 
   seedArray.forEach((id, index) => {
     switch (index) {
       case 0:
+        id > RANGE_MAXIMUM && error(RANGE_ERROR, true);
         deck.push(communications[id]);
         break;
       case 1:
+        id > RANGE_MAXIMUM && error(RANGE_ERROR, true);
         deck.push(force[id]);
         break;
       case 2:
+        id > RANGE_MAXIMUM && error(RANGE_ERROR, true);
         deck.push(finance[id]);
         break;
       case 3:
