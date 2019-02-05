b7a1265ccac80a1d22f16b14a25c4b4fd12035f3
Maas Lalani <maaslalani1@gmail.com>
2019-02-05 11:49:05 -0500

    Create error object

--- commands/load.js
+++ commands/load.js
@@ -2,7 +2,7 @@ const { communications, force, finance, specialInterest } = require('../data/car
 const { underline } = require('cli-color');
 const formatOutput = require('../util/format');
 const error = require('../util/error');
-const { SEED_LENGTH, RANGE_MINIMUM, RANGE_MAXIMUM, RANGE_ERROR, PARSE_ERROR, LENGTH_ERROR, DUPLICATION_ERROR } = require('../util/validation');
+const { SEED_LENGTH, RANGE_MINIMUM, RANGE_MAXIMUM, ERROR } = require('../util/validation');
 
 module.exports = (seed) => {
   console.log(`Your game seed is ${underline(seed)}`);
@@ -10,22 +10,22 @@ module.exports = (seed) => {
   const seedArray = seed.split('');
 
   // Validate game seed
-  seed.length !== SEED_LENGTH && error(LENGTH_ERROR, true);
-  !/^\d+$/.test(seed) && error(PARSE_ERROR, true);
-  seedArray[SEED_LENGTH - 1] == seedArray[SEED_LENGTH - 2] && error(DUPLICATION_ERROR, true);
+  seed.length !== SEED_LENGTH && error(ERROR.LENGTH, true);
+  !/^\d+$/.test(seed) && error(ERROR.PARSE, true);
+  seedArray[SEED_LENGTH - 1] == seedArray[SEED_LENGTH - 2] && error(ERROR.DUPLICATION, true);
 
   seedArray.forEach((id, index) => {
     switch (index) {
       case 0:
-        id > RANGE_MAXIMUM && error(RANGE_ERROR, true);
+        id > RANGE_MAXIMUM && error(ERROR.RANGE, true);
         deck.push(communications[id]);
         break;
       case 1:
-        id > RANGE_MAXIMUM && error(RANGE_ERROR, true);
+        id > RANGE_MAXIMUM && error(ERROR.RANGE, true);
         deck.push(force[id]);
         break;
       case 2:
-        id > RANGE_MAXIMUM && error(RANGE_ERROR, true);
+        id > RANGE_MAXIMUM && error(ERROR.RANGE, true);
         deck.push(finance[id]);
         break;
       case 3:
--- util/validation.js
+++ util/validation.js
@@ -2,9 +2,11 @@ const SEED_LENGTH = 5;
 const RANGE_MINIMUM = 0;
 const RANGE_MAXIMUM = 4;
 
-const RANGE_ERROR = `First three digits of seed must be less than ${RANGE_MAXIMUM}.`;
-const PARSE_ERROR = `Seed must contain only digits.`;
-const LENGTH_ERROR = `Seed must be ${SEED_LENGTH} digits long.`;
-const DUPLICATION_ERROR = `Last two digits of seed cannot be the same.`;
+const ERROR = {
+  RANGE: `First three digits of seed must be less than ${RANGE_MAXIMUM}.`,
+  PARSE: `Seed must contain only digits.`,
+  LENGTH: `Seed must be ${SEED_LENGTH} digits long.`,
+  DUPLICATION: `Last two digits of seed cannot be the same.`,
+}
 
-module.exports = { SEED_LENGTH, RANGE_MINIMUM, RANGE_MAXIMUM, RANGE_ERROR, PARSE_ERROR, LENGTH_ERROR, DUPLICATION_ERROR };
+module.exports = { SEED_LENGTH, RANGE_MINIMUM, RANGE_MAXIMUM, ERROR };
