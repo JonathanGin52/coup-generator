0634a4c95059797c59571e7fde262c9809b2ed96
Maas Lalani <maaslalani1@gmail.com>
2019-02-07 19:38:04 -0500

    Refactor prompt code

--- commands/build.js
+++ commands/build.js
@@ -1,46 +1,13 @@
 const inquirer = require('inquirer');
 const game = require('../util/game');
 
-const getPrompt = () => {
-  // TODO: use method to generate prompt
-  const prompt = [
-    {
-      type: 'list',
-      choices: game['Communications'].cards,
-      message: 'Select a communications influence.',
-      name: 'Communications',
-    },
-    {
-      type: 'list',
-      choices: game['Force'].cards,
-      message: 'Select a force influence.',
-      name: 'Force',
-    },
-    {
-      type: 'list',
-      choices: game['Finance'].cards,
-      message: 'Select a finance influence.',
-      name: 'Finance',
-    },
-    {
-      type: 'list',
-      choices: game['Special Interest'].cards,
-      message: 'Select a special interest influence.',
-      name: 'Special Interest',
-    },
-    {
-      type: 'list',
-      choices: game['Special Interest'].cards,
-      message: 'Select another special interest influence.',
-      name: 'Special Interest',
-    }
-  ];
-  return prompt;
-};
-
 module.exports = () => {
-  inquirer.prompt(getPrompt()).then((answers) => {
-    // console.log(answers);
+  inquirer.prompt(Object.keys(game).map((card) => ({
+      type: 'list',
+      choices: game[card].cards,
+      message: `Select a ${card} influence`,
+      name: card,
+    }))).then((answers) => {
     for (const [type, selection] of Object.entries(answers)) {
       console.log(`${type} ${selection}`);
     }
