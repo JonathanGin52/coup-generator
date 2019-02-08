085fdc1be796063e5552153708ac638affab1a75
Jonathan Gin <jonathangin52@gmail.com>
2019-02-08 10:58:03 -0500

    Fix bug that bug that overwrote special interest

--- commands/build.js
+++ commands/build.js
@@ -1,14 +1,25 @@
 const inquirer = require('inquirer');
 const game = require('../util/game');
 
+const buildPrompt = () => {
+  const prompt = [];
+  for (let card in game) {
+    for (let repetitions = 0; repetitions < game[card].count; repetitions++) {
+      prompt.push({
+        type: 'list',
+        choices: game[card].cards,
+        message: `Select ${repetitions > 0 ? 'another' : 'a'} ${card} influence`,
+        name: `${card}${repetitions > 0 ? ` ${repetitions}` : ''}`,
+      });
+    }
+  }
+  return prompt;
+};
+
 module.exports = () => {
-  inquirer.prompt(Object.keys(game).map((card) => ({
-    type: 'list',
-    choices: game[card].cards,
-    message: `Select a ${card} influence`,
-    name: card,
-  }))).then((answers) => {
+  inquirer.prompt(buildPrompt()).then((answers) => {
     for (const [type, selection] of Object.entries(answers)) {
+      // TODO: build seed
       console.log(`${type} ${selection}`);
     }
   });
