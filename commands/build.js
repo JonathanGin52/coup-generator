17ca852c4e70880585d4078ffeac3c4122f825df
Jonathan Gin <jonathangin52@gmail.com>
2019-02-07 19:45:48 -0500

    Followed eslint indentation guide

--- commands/build.js
+++ commands/build.js
@@ -3,11 +3,11 @@ const game = require('../util/game');
 
 module.exports = () => {
   inquirer.prompt(Object.keys(game).map((card) => ({
-      type: 'list',
-      choices: game[card].cards,
-      message: `Select a ${card} influence`,
-      name: card,
-    }))).then((answers) => {
+    type: 'list',
+    choices: game[card].cards,
+    message: `Select a ${card} influence`,
+    name: card,
+  }))).then((answers) => {
     for (const [type, selection] of Object.entries(answers)) {
       console.log(`${type} ${selection}`);
     }
