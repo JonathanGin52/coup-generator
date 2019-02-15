4bf0fa0c2ede9e80eb1286751a0412a0270ab176
Jonathan Gin <jonathangin52@gmail.com>
2019-02-15 13:49:22 -0500

    Completed build command

--- commands/build.js
+++ commands/build.js
@@ -1,4 +1,5 @@
 const enquirer = require('enquirer');
+const { underline } = require('cli-color');
 const game = require('../util/game');
 
 const buildPrompt = () => {
@@ -11,7 +12,7 @@ const buildPrompt = () => {
         name: `${type}${repetitions > 0 ? ` ${repetitions}` : ''}`,
         choices: data.cards,
         result() {
-          return this.focused.id;
+          return this.focused.id.toString();
         },
       });
     }
@@ -21,11 +22,8 @@ const buildPrompt = () => {
 
 module.exports = () => {
   enquirer.prompt(buildPrompt()).then((answers) => {
-    console.log(answers);
-    for (const [type, selection] of Object.entries(answers)) {
-      // TODO: build seed
-      console.log(`${type} ${selection}`);
-    }
+    const seed = Object.values(answers).join('');
+    console.log(`Your game seed is ${underline(seed)}`);
   });
 };
 
