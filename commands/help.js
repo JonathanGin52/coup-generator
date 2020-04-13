969380d99b56e9d734c881a2548e88f0ec69c02d
Jonathan Gin <jonathangin52@gmail.com>
2020-04-13 01:45:07 -0400

    Remove redundant --seed flag

--- commands/help.js
+++ commands/help.js
@@ -4,9 +4,8 @@ module.exports = () => {
   log('Coup generator possible commands:');
   log('    help                    - Shows possible commands');
   log('    generate                - Generates a random coup game and outputs a seed');
-  log('    load --seed <game-seed> - Loads a coup game from the given seed');
+  log('    load <game-seed>        - Loads a coup game from the given seed');
   log('    explain <role>          - Displays the rules of the searched role(s)');
   log('Optional flags:');
   log('    [--verbose], [-v]       - Displays full description of the role(s)');
-
 };
